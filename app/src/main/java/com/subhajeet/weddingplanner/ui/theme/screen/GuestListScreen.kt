df4ddf72428ed36cdcc20f2ba190b1ad4d721498
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-09-04 21:09:45 +0530

    Top navigation drawer bar implementation setupCommit

--- app/src/main/java/com/subhajeet/weddingplanner/ui/theme/screen/GuestListScreen.kt
+++ app/src/main/java/com/subhajeet/weddingplanner/ui/theme/screen/GuestListScreen.kt
@@ -1,5 +1,6 @@
 package com.subhajeet.weddingplanner.ui.theme.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -14,15 +15,25 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,33 +44,71 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.subhajeet.weddingplanner.Utils.ResultState
 import com.subhajeet.weddingplanner.GuestModel.Guest
 import com.subhajeet.weddingplanner.ui.theme.screen.nav.Routes
 import com.subhajeet.weddingplanner.viewModel.GuestViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GuestListScreen(viewModel: GuestViewModel = hiltViewModel(), navController: NavHostController) {
 
     val guestState = viewModel.guests.collectAsState().value
 
+    // Drawer state
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+
+    val scope =
+        rememberCoroutineScope()
+
     LaunchedEffect(Unit) {
         viewModel.loadGuests()
     }
 
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContents(
+                navController = navController,
+                drawerState = drawerState
+            )
+        }
+    ) {
+
 
     Scaffold(
+
+        topBar = {
+            TopAppBar(
+                title = { Text("Wedding Planner") },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Cyan,   // 🔵 Background color
+                    titleContentColor = Color.White,       // 📝 Title color
+                    navigationIconContentColor = Color.White, // ≡ Menu icon color
+                    actionIconContentColor = Color.White
+                )
+            )
+        },
         floatingActionButton = {
             IconButton(
                 onClick = {
-                    navController.navigate(Routes.AddGuestRoute(
-                        id = null,
-                        name = "",
-                        rsvpStatus = "",
-                    ))
+                    navController.navigate(
+                        Routes.AddGuestRoute(
+                            id = null,
+                            name = "",
+                            rsvpStatus = "",
+                        )
+                    )
                 }
-            ){
+            ) {
                 Icon(
                     imageVector = Icons.Default.Add,
                     contentDescription = "Add Contact",
@@ -67,43 +116,49 @@ fun GuestListScreen(viewModel: GuestViewModel = hiltViewModel(), navController: 
                 )
             }
         }
-    ){innerPadding->
 
-    when (guestState) {
-        is ResultState.Loading -> {
-            Text("Loading guests...", modifier = Modifier.padding(16.dp))
-        }
-        is ResultState.Error -> {
-            Text(
-                text = "Error: ${guestState.message}",
-                color = Color.Red,
-                modifier = Modifier.padding(16.dp)
-            )
-        }
-        is ResultState.Success -> {
-            LazyColumn {
-                items(guestState.data) { guest ->
-                    EachCard(
-                        guest = guest,
-                        onDelete = { viewModel.deleteGuest(guest) },
-                        onClick = {
-                            navController.navigate(
-                                Routes.AddGuestRoute(
-                                    id = guest.id,
-                                    name = guest.name,
-                                    rsvpStatus = guest.rsvpStatus
+
+    ) { innerPadding ->
+
+        when (guestState) {
+            is ResultState.Loading -> {
+                Text("Loading guests...", modifier = Modifier.padding(16.dp))
+            }
+
+            is ResultState.Error -> {
+                Text(
+                    text = "Error: ${guestState.message}",
+                    color = Color.Red,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+            is ResultState.Success -> {
+                LazyColumn (contentPadding = innerPadding){
+                    items(guestState.data) { guest ->
+                        EachCard(
+                            guest = guest,
+                            onDelete = { viewModel.deleteGuest(guest) },
+                            onClick = {
+                                navController.navigate(
+                                    Routes.AddGuestRoute(
+                                        id = guest.id,
+                                        name = guest.name,
+                                        rsvpStatus = guest.rsvpStatus
+                                    )
                                 )
-                            )
-                        }
-                    )
+                            }
+                        )
+                    }
                 }
             }
-        }
-        else -> { // covers any unhandled states
-            Text("Idle...", modifier = Modifier.padding(16.dp))
-        }
-    }
 
+            else -> { // covers any unhandled states
+                Text("Idle...", modifier = Modifier.padding(16.dp))
+            }
+        }
+
+    }
 }
 }
 
@@ -165,6 +220,42 @@ fun EachCard(guest: Guest, onDelete:() -> Unit, onClick:()-> Unit) {
             }
 
 
+        }
+    }
+}
+
+
+@Composable
+fun Drawer(navController: NavController, drawerState: DrawerState) {
+    val scope = rememberCoroutineScope()
+
+    val currentBackStackEntry = navController.currentBackStackEntryAsState()  //for giving color to the navigate screen in the navigation drawer
+    val currentRoute = currentBackStackEntry.value?.destination?.route   //for giving color to the navigate screen in the navigation drawer
+
+    ModalDrawerSheet {
+        Column(Modifier.padding(16.dp)) {
+            listOf(
+                "TabScreen" to Routes.TabScreen,
+                "BudgetCalculatorScreen" to Routes.BudgetCalculatorScreenRoute,
+                "GuestListScreen" to Routes.GuestListScreenRoute
+
+            ).forEach { (label, route) ->
+                val isSelected = currentRoute == route::class.qualifiedName  //for giving color to the navigate screen in the navigation drawer
+                Text(
+                    text = label,
+                    color = if (isSelected) Color.Red else Color.Black, // 🔵 Highlighted text,   //for giving color to the navigate screen in the navigation drawer
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                        .background(if (isSelected) Color(0xFFE0F7FA) else Color.Transparent) // light cyan bg
+                        .clickable {
+                            navController.navigate(route) {
+                                popUpTo(Routes.TabScreen) { inclusive = false }
+                            }
+                            scope.launch { drawerState.close() }
+                        }
+                )
+            }
         }
     }
 }
